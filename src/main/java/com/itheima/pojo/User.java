8f7c0bef03eb7fe22267b852935de0b3b3fe8edf
suibian <123123@qq.com>
2021-08-30 15:14:33 +0800

    项目init

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,14 @@
+package com.itheima.pojo;
+
+/**
+ * @PROJECT_NAME: GitTest
+ * @DESCRIPTION:
+ * @USER: xiguan
+ * @DATE: 2021/8/30 14:53
+ */
+
+
+public class User {
+    private String username;
+    private Integer age;
+}
